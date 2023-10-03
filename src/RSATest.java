143f3cc338fd338a8e2f828c390928d35256086a
Hamdam77 <hamdamisrailov@gmail.com>
2023-10-03 19:47:25 +0300

    Architect Asian development

--- src/RSATest.java
+++ src/RSATest.java
@@ -11,7 +11,7 @@ public class RSATest {
 
 	
 	@Test
-	public void HomePageCheck() throws MalformedURLException
+	public void HomePageCheck1() throws MalformedURLException
 	{
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setBrowserName("firefox");
@@ -21,6 +21,50 @@ public class RSATest {
 		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.139:5555"), caps);
 		driver.get("http://rahulshettyacademy.com");
 		driver.get("http://youtube.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		
+	}
+	@Test
+	public void HomePageCheck2() throws MalformedURLException
+	{
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setBrowserName("firefox");
+		//caps.setPlatform(Platform.);
+		//caps.setCapability(CapabilityType, false);
+		
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.139:5555"), caps);
+		driver.get("http://rahulshettyacademy.com");
+		driver.get("http://youtube.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		
+	}
+	@Test
+	public void HomePageCheck3() throws MalformedURLException
+	{
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setBrowserName("firefox");
+		//caps.setPlatform(Platform.);
+		//caps.setCapability(CapabilityType, false);
+		
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.139:5555"), caps);
+		driver.get("http://rahulshettyacademy.com");
+		driver.get("http://youtube.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
+		driver.get("http://facebook.com");
 		driver.get("http://facebook.com");
 		System.out.println(driver.getTitle());
 		driver.close();
